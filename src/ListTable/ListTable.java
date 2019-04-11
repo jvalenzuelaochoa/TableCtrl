621b95f55cd1125ec1349089d590c903c4f1ffbe
jvalenzuelaochoa <javier.valenzuela@utexas.edu>
2019-04-11 00:15:43 -0500

    Modifing User to work off attributes, to make it easier to modify fields in the input table

--- src/ListTable/ListTable.java
+++ src/ListTable/ListTable.java
@@ -3,6 +3,11 @@ package ListTable;
 import UsrCtrl.User;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import java.util.HashMap;
 
 public class ListTable {
@@ -94,5 +99,48 @@ public class ListTable {
     }
 
     //TODO: Implement test to populate list from file.
+
+    @Test
+    public void populateFromCsv()
+    {
+        String csvFile = "C:\\Users\\jvalenzu\\Documents\\UT\\GradCourses\\AlgorithmicFoundations\\Project\\Sample\\TableCtrl\\src\\ListTable\\shortManualUsers.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        ListTable sampleTbl = new ListTable();
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] currentUsrAtt = line.split(cvsSplitBy);
+                User newUser = new User(Integer.parseInt(currentUsrAtt[0]), currentUsrAtt[1], Integer.parseInt(currentUsrAtt[2]));
+                sampleTbl.addUser(newUser);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        sampleTbl.displayByAttribute(User.userAttributes.ID);
+
+        sampleTbl.displayByAttribute(User.userAttributes.NAME);
+
+        sampleTbl.displayByAttribute(User.userAttributes.SALARY);
+
+    }
 
 }
