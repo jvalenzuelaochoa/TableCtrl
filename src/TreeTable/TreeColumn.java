2905780683393ef7381ea6c9e2db55121bca8f26
jvalenzuelaochoa <javier.valenzuela@utexas.edu>
2019-04-15 18:10:10 -0500

    Adding BFS Insertion and Display Methods

--- src/TreeTable/TreeColumn.java
+++ src/TreeTable/TreeColumn.java
@@ -2,66 +2,62 @@ package TreeTable;
 
 import UsrCtrl.User;
 
+import java.util.HashMap;
+
 public class TreeColumn {
 
     private User usr;
-    private TreeColumn leftById, rightById, leftByName, rightByName, leftBySalary, RightBySalary;
+    HashMap<User.userAttributes, TreeColumn> left;
+    HashMap<User.userAttributes, TreeColumn> right;
+    HashMap<User.userAttributes, TreeColumn> parent;
 
     public TreeColumn( User usr)
     {
         this.usr = usr;
+
+        left = new HashMap<User.userAttributes, TreeColumn>();
+        right = new HashMap<User.userAttributes, TreeColumn>();
+        parent = new HashMap<User.userAttributes, TreeColumn>();
+        for(User.userAttributes att: User.userAttributes.values())
+        {
+            left.put(att, null);
+            right.put(att, null);
+            parent.put(att, null);
+        }
     }
 
-    public TreeColumn getLeftById() {
-        return leftById;
+    public TreeColumn getLeft(User.userAttributes att)
+    {
+        return left.get(att);
     }
 
-    public TreeColumn getLeftByName() {
-        return leftByName;
+    public TreeColumn getRight(User.userAttributes att)
+    {
+        return right.get(att);
     }
 
-    public TreeColumn getLeftBySalary() {
-        return leftBySalary;
-    }
-
-    public TreeColumn getRightById() {
-        return rightById;
-    }
-
-    public TreeColumn getRightByName() {
-        return rightByName;
-    }
-
-    public TreeColumn getRightBySalary() {
-        return RightBySalary;
+    public TreeColumn getParent(User.userAttributes att)
+    {
+        return parent.get(att);
     }
 
     public User getUsr() {
         return usr;
     }
 
-    public void setLeftById(TreeColumn leftById) {
-        this.leftById = leftById;
+    public void setLeft(User.userAttributes att, TreeColumn newLeft)
+    {
+        this.left.replace(att, newLeft);
     }
 
-    public void setLeftByName(TreeColumn leftByName) {
-        this.leftByName = leftByName;
+    public void setRight(User.userAttributes att, TreeColumn newRight)
+    {
+        this.right.replace(att, newRight);
     }
 
-    public void setLeftBySalary(TreeColumn leftBySalary) {
-        this.leftBySalary = leftBySalary;
-    }
-
-    public void setRightById(TreeColumn rightById) {
-        this.rightById = rightById;
-    }
-
-    public void setRightByName(TreeColumn rightByName) {
-        this.rightByName = rightByName;
-    }
-
-    public void setRightBySalary(TreeColumn rightBySalary) {
-        RightBySalary = rightBySalary;
+    public void setParent(User.userAttributes att, TreeColumn newParent)
+    {
+        this.parent.replace(att, newParent);
     }
 
     public void setUsr(User usr) {
--- src/TreeTable/TreeTable.java
+++ src/TreeTable/TreeTable.java
@@ -1,45 +1,112 @@
 package TreeTable;
 
 import UsrCtrl.User;
+import org.junit.Test;
+
+import javax.swing.event.TreeWillExpandListener;
+import java.util.HashMap;
 
 public class TreeTable {
 
-    private TreeColumn rootById, rootByName, rootBySalary;
+    private HashMap<User.userAttributes, TreeColumn> root;
 
     public TreeTable()
     {
-      rootById = null;
-      rootByName = null;
-      rootBySalary = null;
+        root = new HashMap<User.userAttributes, TreeColumn>();
+        for(User.userAttributes att: User.userAttributes.values())
+        {
+            root.put(att, null);
+        }
     }
 
-    public TreeColumn getRootById() {
-        return rootById;
+    public TreeColumn getRoot(User.userAttributes att)
+    {
+        return root.get(att);
     }
 
-    public void setRootById(TreeColumn rootById) {
-        this.rootById = rootById;
+    public void setRoot(User.userAttributes att, TreeColumn root) {
+        this.root.replace(att,root);
     }
 
-    public TreeColumn getRootByName() {
-        return rootByName;
-    }
+    public void addUser(User newUser)
+    {
+        TreeColumn newColumn = new TreeColumn(newUser);
 
-    public void setRootByName(TreeColumn rootByName) {
-        this.rootByName = rootByName;
-    }
+        for(User.userAttributes att: User.userAttributes.values())
+        {
+            if (root.get(att) == null)
+            {
+                root.replace(att, newColumn);
+            }
+            else
+            {
+                TreeColumn traverser = root.get(att);
+                TreeColumn parent = null;
+                boolean newLocationIsLeft = false;
+                while(traverser != null)
+                {
+                    if(traverser.getUsr().attributeIsGreater(att, newUser))
+                    {
+                        parent = traverser;
+                        traverser = traverser.getLeft(att);
+                        newLocationIsLeft = true;
+                    }
+                    else
+                    {
+                        parent = traverser;
+                        traverser = traverser.getRight(att);
+                        newLocationIsLeft = false;
+                    }
+                }
+                newColumn.setParent(att, parent);
+                if(newLocationIsLeft)
+                {
+                    parent.setLeft(att,newColumn);
+                }
+                else
+                    {
+                        parent.setRight(att, newColumn);
+                    }
 
-    public TreeColumn getRootBySalary() {
-        return rootBySalary;
-    }
-
-    public void setRootBySalary(TreeColumn rootBySalary) {
-        this.rootBySalary = rootBySalary;
+            }
+        }
     }
 
     public void displayByAttribute(User.userAttributes att)
     {
+        System.out.println("Tree sorted by: " + att.toString());
+        print("", true, att, root.get(att));
+        System.out.println("\n");
+    }
 
+    private void print(String prefix, boolean isTail, User.userAttributes att, TreeColumn traverser) {
+        System.out.println(prefix + (isTail ? "└── " : "├── ") + traverser.getUsr().toString());
+        if (traverser.getLeft(att) != null)
+        {
+            print(prefix + (isTail ? "   <" : "│  <"), traverser.getRight(att) == null, att, traverser.getLeft(att));
+        }
+
+        if (traverser.getRight(att) != null)
+        {
+            //In binary trees, the right child will always be the tail.
+            print(prefix + (isTail ? "   >" : "│  >"), true, att, traverser.getRight(att));
+        }
+
+    }
+
+    @Test
+    public void notSoQuickTest()
+    {
+        TreeTable sampleTbl = new TreeTable();
+        sampleTbl.addUser( new User(1, "Jesus", 3500));
+        sampleTbl.addUser( new User(2, "Pedro", 3500));
+        sampleTbl.addUser( new User(6, "Ochoa", 3500));
+        sampleTbl.addUser( new User(5, "Valenzuela", 3500));
+        sampleTbl.addUser( new User(3, "Mentira", 3500));
+        sampleTbl.addUser( new User(4, "Javier", 3500));
+        sampleTbl.displayByAttribute(User.userAttributes.ID);
+        sampleTbl.displayByAttribute(User.userAttributes.NAME);
+        sampleTbl.displayByAttribute(User.userAttributes.SALARY);
     }
 
 }
